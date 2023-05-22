9687cc3e32c1c5878ff535318d2f5e77805fdc9a
eleoes <leo.escaroz@gmail.com>
2023-05-22 10:15:37 -0700

    all products page, wip: single product page

--- components/NewProducts.js
+++ components/NewProducts.js
@@ -1,23 +1,12 @@
 import styled from "styled-components";
 import Center from "@/components/Center";
-import ProductBox from "@/components/ProductBox";
-
-const ProductsGrid = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    gap: 40px;
-    padding-top: 20px;
-`;
+import ProductsGrid from "@/components/ProductsGrid";
 
 export default function NewProducts({products}) {
     return (
         <Center>
             <h2>New Arrivals</h2>
-            <ProductsGrid>
-                {products?.length > 0 && products.map(product => (
-                    <ProductBox {...product}/>
-                ))}
-            </ProductsGrid>
+            <ProductsGrid products={products}/>
         </Center>
     );
 }
